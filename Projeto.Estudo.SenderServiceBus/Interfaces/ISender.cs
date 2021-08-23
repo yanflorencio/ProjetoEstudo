0490bc529529eec49ab592cdd9b13c6fbf6c92b4
Yan Florencio do Nascimento <yanflorencio@hotmail.com>
2021-08-23 20:48:13 -0300

    Criando projeto para envio de mensagens ao Service Bus

---
+++ Projeto.Estudo.SenderServiceBus/Interfaces/ISender.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Projeto.Estudo.SenderServiceBus.Interfaces
+{
+	public interface ISender
+	{
+		public Task SendMessageAsync<T>(T message, string topicName);
+	}//class
+}//namespace
---
+++ Projeto.Estudo.SenderServiceBus/Sender.cs
@@ -0,0 +1,34 @@
+﻿using Azure.Messaging.ServiceBus;
+using Newtonsoft.Json;
+using Projeto.Estudo.SenderServiceBus.Interfaces;
+using ProjetoEstudo.Utils;
+using System.Threading.Tasks;
+
+namespace Projeto.Estudo.SenderServiceBus
+{
+	public class Sender : ISender
+	{
+
+		public Sender()
+		{
+
+		}
+
+		public async Task SendMessageAsync<T>(T message, string queueOrTopicName)
+		{
+			string connectionString = UtilitiesConfig.GetAppSetting("AzureServiceBus");
+
+			await using (ServiceBusClient cliente = new ServiceBusClient(connectionString))
+			{
+				ServiceBusSender serviceBusSender = cliente.CreateSender(queueOrTopicName);
+
+				string jsonMessage = JsonConvert.SerializeObject(message);
+
+				ServiceBusMessage serviceBusMessage = new ServiceBusMessage(jsonMessage);
+
+				await serviceBusSender.SendMessageAsync(serviceBusMessage);
+			}//using
+		}//func
+
+	}//class
+}//namespace
